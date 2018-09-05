7fbdd48d8011447b92a47339d7dd3e42bd9e35f3
Ashraf Uthman <ashrafelion@gmail.com>
2018-09-05 18:33:51 +0200

    Deep Understanding How React works

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,14 +4,26 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit.js';
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: 'asfa1', name: 'Max', age: 28 },
-      { id: 'vasdf1', name: 'Manu', age: 29 },
-      { id: 'asdf11', name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value',
-    showPersons: false
+  constructor(props) {
+    super(props);
+    this.state = {
+        persons: [
+          { id: 'asfa1', name: 'Max', age: 28 },
+          { id: 'vasdf1', name: 'Manu', age: 29 },
+          { id: 'asdf11', name: 'Stephanie', age: 26 }
+        ],
+      otherState: 'some other value',
+      showPersons: false
+    }
+    console.log('[App.js] Inside constructor', props);
+  }
+ 
+  componentWillMount () {
+    console.log('[App.js] Inside componentWillMount');
+  }
+  
+  componentDidMount () {
+    console.log('[App.js] Inside componentDidMount');
   }
 
   nameChangedHandler = ( event, id ) => {
@@ -46,7 +58,7 @@ class App extends Component {
   }
 
   render () {
-
+    console.log('[App.js] Inside render');
     let persons = null;
 
     if ( this.state.showPersons ) {
